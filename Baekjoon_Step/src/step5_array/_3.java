91af589aac9670bb37fd3af273c427a774628d27
NamYunji <dbs7wl7@gmail.com>
2021-02-14 20:18:46 +0900

    Baekjoon ch5

--- Baekjoon_Step/src/step5_array/_3.java
+++ Baekjoon_Step/src/step5_array/_3.java
@@ -1,8 +1,27 @@
 package step5_array;
 
+import java.util.Scanner;
+
 public class _3 {
 
-		
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        sc.close();
+         
+        int[] counts = new int[10];
+        int number = A * B * C;
+        while (number > 0) {
+            counts[number % 10]++;
+            number /= 10;
+        }
+         
+        for (int i = 0; i < counts.length; ++i) {
+            System.out.println(counts[i]);
+        }
+    }		
 	
 	}
 
